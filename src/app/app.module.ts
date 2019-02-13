e0ba6c9c25093c8c365c1624d0864ae56d052941
Marcus Santos <marcus.santos@geohub.com.br>
2019-02-13 19:42:42 -0200

    Add slide intro page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,16 @@ import { TabsPage } from "../pages/tabs/tabs";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { FeedPageModule } from "../pages/feed/feed.module";
+import { IntroPageModule } from "../pages/intro/intro.module";
 
 @NgModule({
   declarations: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp), FeedPageModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    FeedPageModule,
+    IntroPageModule
+  ],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, AboutPage, ContactPage, HomePage, TabsPage],
   providers: [
